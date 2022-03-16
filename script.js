3bbab67dcb6560d709a9221a4495077ee9967312
SYSHIL <ilhan.syed@gmail.com>
2022-03-16 16:42:32 +0530

    UPDATE sounds

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-window.addEventListener('keydown',function(e){
+function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if(!audio) return;
-    audio.currentTime = 0;
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if(!audio) return; // if no audio element selected get out of function
+    audio.currentTime = 0; // we play from beginning of audio clip
     audio.play();
-    if(!key) return;
-    key.classList.add('playing');
-    
-});
+    key.setAttribute("class","key playing");
+}
+function removeTransition(e){
+        if(e.propertyName !== "transform") return;
+        this.classList.remove("playing");
+}
+ 
+//transition end fires after a certain number of seconds whose value is given in the transition property in stylesheet file
+window.addEventListener('keydown',playSound);
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend",removeTransition));
+
+
+
